18aea85432e4e6f386893e9c7ac6f02139911e13
Shubham Jaiswal <jaiswalshubham31012000@gmail.com>
2023-12-27 17:37:24 +0530

    menu project complete

--- src/Menu.jsx
+++ src/Menu.jsx
@@ -1,20 +1,51 @@
+import { useState } from 'react';
 import MenuItem from './MenuItem';
 
 const Menu = ({ menu }) => {
+  const [category, setCategory] = useState(menu);
+
+  const handleAll = () => {
+    setCategory(menu);
+  };
+
+  const handleBreakfast = () => {
+    const breakfast = menu.filter((item) => item.category === 'breakfast');
+    setCategory(breakfast);
+  };
+
+  const handleLunch = () => {
+    const lunch = menu.filter((item) => item.category === 'lunch');
+    setCategory(lunch);
+  };
+
+  const handleShakes = () => {
+    const shakes = menu.filter((item) => item.category === 'shakes');
+    setCategory(shakes);
+  };
+
   return (
     <>
       <div className="btn-container">
-        <button className="btn">all</button>
-        <button className="btn">breakfast</button>
-        <button className="btn">lunch</button>
-        <button className="btn">shakes</button>
+        <button className="btn" onClick={handleAll}>
+          all
+        </button>
+        <button className="btn" onClick={handleBreakfast}>
+          breakfast
+        </button>
+        <button className="btn" onClick={handleLunch}>
+          lunch
+        </button>
+        <button className="btn" onClick={handleShakes}>
+          shakes
+        </button>
       </div>
       <div className="section-center">
-        {menu.map((item) => {
+        {category.map((item) => {
           return <MenuItem key={item.id} {...item} />;
         })}
       </div>
     </>
   );
 };
+
 export default Menu;
